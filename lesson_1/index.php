5f0ea4315ca2ceb8906080ba341f22cb46da9b09
Efrony <efron.vit@gmail.com>
2019-04-21 13:50:14 +0300

    исправление дз lesson_1

--- lesson_1/index.php
+++ lesson_1/index.php
@@ -5,29 +5,31 @@
     var_dump((int)'012345');     // 12345  - строка приводится к целочисленному значению. 
     var_dump((float)123.0 === (int)123.0); //  false - потому что === стравнивает по типу данных float !== int
     var_dump((int)0 === (int)'hello, world'); // true - строка приводится в целочисленный тип данных, т.к. в ней нет первой цифры. она равна 0.
+?>
 
-
+<?php
  // ЗАДАНИЕ 4.
     $title = "HOME PAGE";
     $h1 = '<h1>MY FIRST PHP-CODE</h1>';
     $dateToday = getDate();
-    $homePage ='
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>' . $title . '</title> 
-    </head>
-    <body>'
-    . $h1 . '<br>' . $dateToday['year'] .
-    '</body>
-    </html>';
-    
-echo $homePage ;
+?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title> <?=$title?> </title> 
+</head>
+<body>
+     <?=$h1?><br> <?=$dateToday['year']?>
+
+</body>    
+</html>
+
+<?php 
 // 5. *Используя только две переменные, поменяйте их значение местами. 
 $a = 10;
 $b = 20;
 $a = $a + $b ; // 30
 $b = $a - $b ; // 10
 $a = $a - $b ; // 20
+?>
